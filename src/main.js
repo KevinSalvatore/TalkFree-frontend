2ffb36b58f368b2d60ac0e9b049ad81600eb12b7
AJie <kevinsalvatore@qq.com>
2019-07-06 22:03:01 +0800

    Encapsulates local storage API, and encapsulates axios!

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+import Vuex from "vuex";
+import "es6-promise/auto";
+import "animate.css";
 
 import "./utils/rem";
-
 import "./style/reset.css";
-import "animate.css";
 import "./style/more.css";
-
 import "./mixin/index.styl";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   el: "#app",
